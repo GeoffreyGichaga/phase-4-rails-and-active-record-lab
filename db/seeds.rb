7518bfdbfee6839445aceb03c717bb3feca09ce1
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-07-18 12:45:49 +0300

    commit tree 16d6f91cdebe1f3cea515d85493a9d24fa5be119

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create([
+    {
+        first_name:"Jeff",
+        last_name:"Githinji",
+        grade:98
+    
+    },
+    {
+        first_name:"TJ",
+        last_name:"JJ",
+        grade:95
+    
+    },
+    {
+        first_name:"Geoff",
+        last_name:"TJ",
+        grade:99
+    
+    },
+    {
+        first_name:"Mr.G",
+        last_name:"Tj",
+        grade:96
+    
+    },
+    {
+        first_name:"Jefferson",
+        last_name:"Githinji",
+        grade:95
+    
+    }
+])
